ee6885c4e22366fb1dd337b3d859640494e1fc03
Audrey Lopez <audreyelopez@gmail.com>
2014-10-14 19:42:45 -0500

    Changes to database.php to make private any user sign-in or password sensitive information; Set enviroment detection in start.php file to detect when in local enviroment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,15 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('homestead'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+ 
+    if (!empty($_SERVER['LARAVEL_ENV'])) {
+        return $_SERVER['LARAVEL_ENV'];
+    }
+ 
+    return 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
